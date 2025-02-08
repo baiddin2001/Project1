641b26b8e9c8720c49c9db5840dcd94c7d89f10b
baiddin2001 <201980350@palawan.edu.ph>
2025-02-08 11:22:12 +0800

    Adding class button in the Student login page

--- Ambot/login.php
+++ Ambot/login.php
@@ -2,8 +2,8 @@
 <?php
 include_once 'components/connect.php';
 ?>
-<?php
 
+<?php
 if(isset($_COOKIE['user_id'])){
    $user_id = $_COOKIE['user_id'];
 }else{
@@ -11,7 +11,6 @@ if(isset($_COOKIE['user_id'])){
 }
 
 if(isset($_POST['submit_login'])){
-
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
@@ -27,25 +26,18 @@ if(isset($_POST['submit_login'])){
    }else{
       $message[] = 'incorrect email or password!';
    }
-
 }
 ?>
 
-<!-- this is for the sign up -->
 <?php
-if(isset($_COOKIE['user_id'])){
-   $user_id = $_COOKIE['user_id'];
-}else{
-   $user_id = '';
-}
-
 if(isset($_POST['submit_signup'])){
-
    $id = unique_id();
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $email = $_POST['email'];
    $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $class = $_POST['class'];
+   $class = filter_var($class, FILTER_SANITIZE_STRING);
    $pass = sha1($_POST['pass']);
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
    $cpass = sha1($_POST['cpass']);
@@ -66,10 +58,10 @@ if(isset($_POST['submit_signup'])){
       $message[] = 'email already taken!';
    }else{
       if($pass != $cpass){
-         $message[] = 'confirm passowrd not matched!';
+         $message[] = 'confirm password not matched!';
       }else{
-         $insert_user = $conn->prepare("INSERT INTO `users`(id, name, email, password, image) VALUES(?,?,?,?,?)");
-         $insert_user->execute([$id, $name, $email, $cpass, $rename]);
+         $insert_user = $conn->prepare("INSERT INTO `users`(id, name, email, class, password, image) VALUES(?,?,?,?,?,?)");
+         $insert_user->execute([$id, $name, $email, $class, $cpass, $rename]);
          move_uploaded_file($image_tmp_name, $image_folder);
          
          $verify_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ? LIMIT 1");
@@ -82,9 +74,7 @@ if(isset($_POST['submit_signup'])){
          }
       }
    }
-
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -94,88 +84,79 @@ if(isset($_POST['submit_signup'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Student Login</title>
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
-   <!-- custom css file link  -->
    <link rel="stylesheet" href="css/login_register1.css">
-
-
-
 </head>
-
 <body>
-
-</head>
 <div class="container" id="container">
-	<div class="form-container sign-up-container">
-    
-		<form action="" method="post" enctype="multipart/form-data">
-			<h1>Create Account</h1>
-            <span class= "span1" style="margin-bottom: 10px;">Create your PTCI account and begin your academic journey today!</span>
-			<input type="text" name="name" placeholder="Enter your Name" maxlength="50" required >
-            <input type="email" name="email" placeholder="Enter your Email" maxlength="100" required >
-			<input type="password" name="pass" placeholder="Enter your Password" maxlength="20" required >
-            <input type="password" name="cpass" placeholder="Confirm your Password" maxlength="20" required >
-            <span  class="profile_pic">Please select your photo <span>*</span></span>
-            <input type="file" name="image" accept="image/*" required >
-			<button style="margin-top: 5px;" type ="submit" name="submit_signup" value="Register Now">Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form action="" method="post" enctype="multipart/form-data" class="login">
-			<h1>Welcome!</h1>
-			<span>Log in using your email and password.</span>
-			<input type="email" name="email" placeholder="Enter your Email" maxlength="100" required >
-            <input type="password" name="pass" placeholder="Enter your Password" maxlength="20" required >
-			<!-- <button><input type="submit" name="submit" value="login now"></button> -->
-             <br>
-            <button type="submit" name="submit_login" value="login now" >Login Now</button>
-            <p class="link">Are you an Instructor? <a href="admin/Login.php" style="color: blue;">Login here</a></p>
-            
-		</form> 
-	</div>
-	<div class="overlay-container">
-   
-		<div class="overlay">
-              <div class="back_ptci-logo_right"></div>
-              <div class="back_ptci-logo_left"></div>
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Log In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hello, Student!</h1>
-				<p>Don't have an account yet? Sign up now! and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
+   <div class="form-container sign-up-container">
+      <form action="" method="post" enctype="multipart/form-data">
+         <h1>Create Account</h1>
+         <span class="span1">Create your account and begin your academic journey today!</span>
+         <input type="text" name="name" placeholder="Enter your Name" maxlength="50" required>
+         <input type="email" name="email" placeholder="Enter your Email" maxlength="100" required>
+         <select name="class" required>
+            <option value="">Select Your Class</option>
+            <option value="ICT">ICT</option>
+            <option value="HUMMS">HUMMS</option>
+         </select>
+         <input type="password" name="pass" placeholder="Enter your Password" maxlength="20" required>
+         <input type="password" name="cpass" placeholder="Confirm your Password" maxlength="20" required>
+         <span class="profile_pic">Please select your photo <span>*</span></span>
+         <input type="file" name="image" accept="image/*" required>
+         <button type="submit" name="submit_signup">Sign Up</button>
+      </form>
+   </div>
+   <div class="form-container sign-in-container">
+      <form action="" method="post" class="login">
+         <h1>Welcome!</h1>
+         <span>Log in using your email and password.</span>
+         <input type="email" name="email" placeholder="Enter your Email" maxlength="100" required>
+         <input type="password" name="pass" placeholder="Enter your Password" maxlength="20" required>
+         <button type="submit" name="submit_login">Login Now</button>
+         <p class="link">Are you an Instructor?  
+         <a href="admin/Login.php" style="color: blue;">Login here</a>
+         </p>
+      </form> 
+   </div>
+   <div class="overlay-container">
+      <div class="overlay">
+         <div class="overlay-panel overlay-left">
+            <h1>Welcome!</h1>
+            <p>To keep connected with us please login with your personal info</p>
+            <button class="ghost" id="signIn">Log In</button>
+         </div>
+         <div class="overlay-panel overlay-right">
+            <h1>Hello, Student!</h1>
+            <p>Don't have an account yet? Sign up now!</p>
+            <button class="ghost" id="signUp">Sign Up</button>
+         </div>
+      </div>
+   </div>
 </div>
+
+<style>
+   select {
+    background-color: #eee;
+    border: none;
+    padding: 12px 15px;
+    margin: 8px 0;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 10px;
+    font-size: 14px;
+}
+</style>
+
 <script>
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
-
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
-
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-    });
+   document.getElementById('signUp').addEventListener('click', () => {
+      document.getElementById('container').classList.add("right-panel-active");
+   });
+   document.getElementById('signIn').addEventListener('click', () => {
+      document.getElementById('container').classList.remove("right-panel-active");
+   });
 </script>
-
 <footer class="footer1">
-   &copy; copyright @ <?= date('Y'); ?> by Palawan Technological College Inc. | all rights reserved!
-
+   &copy; <?= date('Y'); ?> by Palawan Technological College Inc. | All rights reserved!
 </footer>
-
-<!-- custom js file link  -->
-
-   
 </body>
 </html>
